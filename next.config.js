cebd2dcb496107ef4c59d5eb6e48915522fce19a
Sofia zaiz <sofiazaiz@Air-de-Sofia.lan>
2025-12-13 07:41:39 +0100

    Fix Next.js image domains for Supabase

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   i18n: {
     locales: ["fr", "en", "es", "de", "pt", "nl", "ar"],
     defaultLocale: "fr"
-  }
+  },
+
+  images: {
+    domains: [
+      "dmpbxpltbmmoniokthme.supabase.co"
+      ],
+    }, 
 };
 
 module.exports = nextConfig;
